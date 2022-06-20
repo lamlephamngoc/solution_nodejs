9dc8b1dae5a2bb3b067c27c5a2e1d508faec457b
Lam Le <lamle@gmx.com>
2022-06-20 15:36:11 +0700

    remove test

--- src/__test__/CsvProcessor.test.ts
+++
@@ -1,9 +0,0 @@
-import {expect} from 'chai';
-import CsvProcessor from '../CsvProcessor';
-
-const csvProcessor = new CsvProcessor('csv/transactions.csv');
-
-test('should return latest portfolio all token in USD', () => {
-  // then
-  csvProcessor.process();
-});
